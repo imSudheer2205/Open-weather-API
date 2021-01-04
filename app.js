9c5d3cd384c858d4e76d4ef68cf6fb9bccad01ce
imSudheer2205 <44637196+imSudheer2205@users.noreply.github.com>
2021-01-04 17:22:08 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+var input = document.querySelector('.input_text');
+var main = document.querySelector('#name');
+var temp = document.querySelector('.temp');
+var desc = document.querySelector('.desc');
+var clouds = document.querySelector('.clouds');
+var button= document.querySelector('.button');
+
+
+button.addEventListener('click', function(name){
+fetch('http://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=a81306c53cb2a37d575fc60e81f26a13')
+.then(response => response.json())
+.then(data => {
+  var tempValue = data['main']['temp'];
+  var nameValue = data['name'];
+  var descValue = data['weather'][0]['description'];
+
+  main.innerHTML = nameValue;
+  desc.innerHTML = "Desc - "+descValue;
+  temp.innerHTML = "Temp - "+tempValue;
+  input.value ="";
+
+})
+
+.catch(err => alert("Wrong city name!"));
+})
